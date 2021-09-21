0ea35b7815e08e9299c38ac711dab6b9e6544fd6
mcmonkey314 <dtrybula@gmail.com>
2021-09-21 18:27:41 +0000

    update dir change

--- github/push_it.py
+++ github/push_it.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import os 
 commit_msg = input('Commit Comment:')
-working_dir = 'cd ~/mycode/'
+working_dir = '/home/student/mycode'
 git_add = 'git add *'
 git_commit = 'git commit -m "'+commit_msg+'"'
 git_push = 'git push origin'
 
-os.system(working_dir)
+os.chdir(working_dir)
+#os.system(working_dir)
 os.system(git_add)
 os.system(git_commit)
 os.system(git_push)
