584b77937834d7bbf2269a173078d91a2bf9b732
mcmonkey314 <dtrybula@gmail.com>
2021-09-23 15:48:34 +0000

    crayons

---
+++ netfacd/interface_reader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import netifaces
+
+print(netifaces.interfaces())
+for i in netifaces.interfaces():
+    print('\n**************Details of Interface - ' + i + ' *********************')
+    try:
+      #print(netifaces.ifaddresses(i))
+      print('MAC: ',end='')
+      print(netifaces.ifaddresses(i)[netifaces.AF_LINK][0]['addr'])
+      print('IP:  ',end='')
+      print(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+    except:
+      print('could not collect adapter information')
+
+
